02042af64369243794351603236a151f206602b6
AnthoniVignoly <115472326+AnthoniVignoly@users.noreply.github.com>
2022-11-11 19:30:04 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,10 +3,13 @@ using namespace std;
 
 int main(){
 //Qui vanno definite le variabili
-
+  float a,b,c;
 cin >> a >> b >> c;
 
 //Qui va scritto il codice per eseguire i calcoli richiesti
-
+   cout << "Area triangolo : " << ((a*b)/2) << endl;
+   cout << "Area quadrato : " << (a*a) << endl;
+   cout << "Area rettangolo : " << (a*b) << endl;
+   cout << "Area trapezio : " << (((a+b)*c)/2) << endl;
 
 }
